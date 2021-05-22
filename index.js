34c13efa38cdfc8bd7107bef8202fb40be0a6a76
NOT-SWong <sophia.wong22@sitechhs.com>
2021-05-21 23:17:57 -0400

    finally made up~

--- index.js
+++ index.js
@@ -76,15 +76,39 @@ const titles = [
 
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
-
+const wroteIn = books.map( (books) => books.authorFirst + " " + books.authorLast + ` wrote ` + `"` + books.name + `"` + ` in ` + books.publishDate);
+console.log(wroteIn);
 //Sort books from oldest to most recent
-
+const ordered = books.sort((a,b) => (a.publishDate > b.publishDate ? 1 : -1));
+console.table(ordered);
 //sort books alphabetically
-
+const alphabetical = books.sort((a,b) => {
+  let fa = a.name.toLowerCase();
+  let fb = b.name.toLowerCase();
+  return fa < fb ? -1 : fa > fb ? 1 : 0;
+});
 //Find who wrote War and Peace
-
+const WnP = books.find(names => {
+  if (names.name === "War and Peace") {
+    return true;
+  }
+});
+console.log(WnP.authorFirst + ` ` + WnP.authorLast);
 //how many books were written before 1900?
-
+const prior = books.filter(books => {
+  if (books.publishDate < 1900) {
+    return true;
+  }
+});
+console.log(prior.length);
 //was there at least one book published within the last 100 years?
-
+const within = books.filter((books) => books.publishDate > 1921);
+console.log(within);
 //was every book published within the last 100 years?
+const every = books.every(function (book) {
+  const currentYear = new Date().getFullYear();
+  if (currentYear - book.publishDate <= 100) {
+    return true;
+  }
+});
+console.log(every);
